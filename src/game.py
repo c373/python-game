48ef2a3210fe66f5be291b1c3ba8c9b906da06d8
c373 <76661778+c373@users.noreply.github.com>
2022-07-10 19:30:49 -0700

    Abstract the grid drawing

--- src/game.py
+++ src/game.py
@@ -4,14 +4,33 @@ pygame.init()
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 
+# in pixels
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+
+# size of each cell
+CELL_SIZE = 20
+
+# width and height in number of cells
+GRID_WIDTH, GRID_HEIGHT = 20, 20
+
+# center the grid on the screen
+GRID_X, GRID_Y = (SCREEN_WIDTH / 2) - (GRID_WIDTH * CELL_SIZE) / 2, (SCREEN_HEIGHT / 2) - 50 - (GRID_HEIGHT * CELL_SIZE) / 2
+
+# create the displayable screen, its a surface that we can draw to
 screen = pygame.display.set_mode((800, 600))
+running = True
 
-while 1:
+while running:
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT: running = False
 
+    if not running: sys.exit()
+
+    # clear the screen with black
     screen.fill(BLACK)
 
-    grid.drawGrid(screen, WHITE, 20, 20, 20, 20, 20)
+    grid.drawGrid(screen, WHITE, GRID_X, GRID_Y, GRID_WIDTH, 20, CELL_SIZE)
 
+    # flush the buffer and display on the screen
     pygame.display.flip()
